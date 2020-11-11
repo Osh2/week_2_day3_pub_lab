071b43c8379dd3856c042617fd582ed5bc4ba77e
Nargis <nargis.salim@btinternet.com>
2020-11-11 13:49:36 +0000

    added drinks and 2 tests

--- src/pub.py
+++ src/pub.py
@@ -1,6 +1,9 @@
 class Pub:
-    def __init__(self, name, cash):
+    def __init__(self, name, cash, drinks):
         self.name = name
         self.cash = cash
+        self.drinks = drinks
+
+        
        
     
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -3,7 +3,15 @@ from src.pub import Pub
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.pub = Pub("Kings Head", 1000)
+        self.pub = Pub("Kings Head", 1000, ["whisky", "vodka", "lager", "cider"])
 
     def test_pub_has_name(self):
         self.assertEqual("Kings Head", self.pub.name)
+
+    def test_pub_has_money(self):
+        self.assertEqual(1000, self.pub.cash)
+
+    def test_has_drinks(self):
+        self.assertEqual(["whisky", "vodka", "lager", "cider"], self.pub.drinks)
+        
+    
